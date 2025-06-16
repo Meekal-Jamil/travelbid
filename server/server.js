e40452bf673070d09814f5b7e3a9a6a0a7418b3a
Meekal-Jamil <meekalstudent@gmail.com>
2025-06-17 00:15:14 +0500

    Final touches

--- server/server.js
+++ server/server.js
@@ -43,4 +43,4 @@ app.use((err, req, res, next) => {
 connectDB();
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
